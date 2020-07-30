0535ceead876d9cb6bdc62b2d312fd7f769b76de
Akhilesh <akhilesh.kc@dxc.com>
2020-07-30 21:01:51 +0530

    second commit

--- welcome.java
+++ welcome.java
@@ -1,5 +1,8 @@
  class Welcome{
 public static void main(String [] args){
 System.out.println("welcome to java git jenkins combo");
+for(int i= 0 ; i<10; i++){
+System.out.println(i+ " -Welcome");
+}
 }
 }
